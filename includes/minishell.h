d45366b1a79e8db50353dda4f878532e5a393000
JorFik <138692812+JorFik@users.noreply.github.com>
2024-03-12 20:14:15 +0100

    Added libraries for allowed functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 08:43:00 by tunsal            #+#    #+#             */
-/*   Updated: 2024/03/12 16:07:42 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/03/12 19:42:26 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
 
 #endif
