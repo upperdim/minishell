b9260f1eee53fd7969aaf7afca9205e1062bb056
JorFik <138692812+JorFik@users.noreply.github.com>
2024-03-14 19:33:13 +0100

    Added Libraries

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 08:43:00 by tunsal            #+#    #+#             */
-/*   Updated: 2024/03/12 19:42:26 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/03/14 16:41:30 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+# include <sys/stat.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <signal.h>
+# include <dirent.h>
+# include <sys/ioctl.h>
+# include <termios.h>
 
 #endif
