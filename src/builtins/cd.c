bf60eb049741b43e0e970cd1db213d60b0872360
JorFik <138692812+JorFik@users.noreply.github.com>
2024-03-22 19:05:11 +0100

    Fixing error output for cd
    Ereased extra '\n' and added missing  ' '

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 13:28:29 by JFikents          #+#    #+#             */
-/*   Updated: 2024/03/22 18:09:57 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/03/22 19:03:50 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,9 @@ void	cd(char *input)
 		status = chdir(getenv("HOME"));
 	if (status == -1)
 	{
-		ft_putstr_fd("minishell: cd:", 2);
+		ft_putstr_fd("minishell: cd: ", 2);
 		ft_putstr_fd(input, 2);
 		ft_putstr_fd(": ", 2);
 		perror(NULL);
-		ft_putstr_fd("\n", 2);
 	}
 }
