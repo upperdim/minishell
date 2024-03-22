cd7cbaf4b515caff17110aed13e52563f0564cc1
JorFik <138692812+JorFik@users.noreply.github.com>
2024-03-22 19:26:14 +0100

    Solved issue #10 False positive for echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 14:57:51 by JFikents          #+#    #+#             */
-/*   Updated: 2024/03/22 18:00:35 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/03/22 19:23:27 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 void	echo(char *input)
 {
+	int		space_is_present;
+
+	space_is_present = 0;
 	while (*input == ' ')
+	{
 		input++;
-	if (*input)
+		space_is_present = 1;
+	}
+	if (*input && space_is_present)
 		ft_putstr_fd(input, 1);
+	else if (*input)
+	{
+		ft_putstr_fd("minishell: echo", 2);
+		ft_putstr_fd(input, 2);
+		ft_putstr_fd(": command not found", 2);
+	}
 	ft_putstr_fd("\n", 1);
 }
