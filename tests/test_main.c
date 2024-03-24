a1315fec1df38854ecbe04beb7682c8e922ce23b
JorFik <138692812+JorFik@users.noreply.github.com>
2024-03-24 15:57:55 +0100

    test_builtins() created

--- tests/test_main.c
+++ tests/test_main.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 12:26:18 by JFikents          #+#    #+#             */
-/*   Updated: 2024/03/24 12:34:07 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/03/24 15:53:29 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,35 @@ static void	compile_minishell(void)
 	waitpid(pid, NULL, 0);
 }
 
+static void	test_builtins(void)
+{
+	pid_t	pid;
+	int		pipe_in[2];
+	int		fd_out;
+	int		check_pipe;
+
+	fd_out = open("tests/input.txt", O_RDWR | O_CREAT | O_TRUNC, 0644);
+	check_pipe = pipe(pipe_in);
+	pid = fork();
+	if (pid == -1)
+		exit(EXIT_FAILURE);
+	if (!pid)
+	{
+		dup2(fd_out, STDOUT_FILENO);
+		ft_close(&fd_out);
+		setup_in_pipe(pipe_in);
+		ft_execve((char *[]){"./minishell_builtins", NULL}, NULL, NULL);
+	}
+	waitpid(pid, NULL, WUNTRACED);
+	ft_close(&pipe_in[PIPE_FD_READ]);
+	ft_close(&pipe_in[PIPE_FD_WRITE]);
+	ft_close(&fd_out);
+}
+
 int	main(void)
 {
 	compile_minishell();
+	test_builtins();
+	unlink("tests/input.txt");
 	return (0);
 }
