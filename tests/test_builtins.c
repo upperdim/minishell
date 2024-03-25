76b6e1e84ca2abd20e6840dc9b3eddf07d5255d2
JorFik <138692812+JorFik@users.noreply.github.com>
2024-03-25 16:09:55 +0100

    Added feedback, and tests for env builtins

--- tests/test_builtins.c
+++ tests/test_builtins.c
@@ -6,13 +6,13 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 19:49:01 by JFikents          #+#    #+#             */
-/*   Updated: 2024/03/25 14:34:21 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/03/25 15:57:16 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-static pid_t	start_minishell(int *pipe_write)
+static pid_t	start_minishell_builtins(int *pipe_write)
 {
 	pid_t	pid;
 	int		pipe_in[2];
@@ -41,16 +41,19 @@ static pid_t	start_minishell(int *pipe_write)
 
 void	test_builtins(void)
 {
-	int		write_minishell;
-	int		read_output_fd;
-	pid_t	pid;
-	int		status;
-	char	*line;
-	char	*prompt;
-	char	*cwd;
-	char	*echo_line;
+	extern char	**environ;
+	pid_t		pid = 0;
+	char		*cwd = NULL;
+	char		*line = NULL;
+	char		*prompt = NULL;
+	char		*tmp_line = NULL;
+	char		*echo_line = NULL;
+	int			write_minishell = 0;
+	int			read_output_fd = 0;
+	int			status = 0;
+	int			i = 0;
 
-	pid = start_minishell(&write_minishell);
+	pid = start_minishell_builtins(&write_minishell);
 
 
 //_ COMMANDS FOR MINISHELL _//
@@ -63,6 +66,7 @@ void	test_builtins(void)
 	ft_putendl_fd("echo -n Hello World", write_minishell);
 	ft_putendl_fd("echo Hello World-n", write_minishell);
 	ft_putendl_fd("echo Hello World -n", write_minishell);
+	ft_putendl_fd("env", write_minishell);
 	system("leaks minishell_builtins");
 	ft_putendl_fd("exit 123", write_minishell);
 	usleep(100000);
@@ -72,17 +76,6 @@ void	test_builtins(void)
 
 	read_output_fd = open("tests/minishell_builtins.out", O_RDONLY);
 
-	//_ CHECKING EXIT STATUS TEST 1 _//
-	if (WIFEXITED(status))
-	{
-		if (WEXITSTATUS(status) == 123)
-			ft_putendl_fd(GREEN"Test 1 exit status success", 1);
-		else
-			ft_putendl_fd(RED"Test 1 exit status failed", 1);
-	}
-	else
-		ft_putendl_fd("exit status is incorrect", 1);
-
 
 	//_ CHECKING PROMPT TEST 1_//
 	line = get_next_line(read_output_fd);
@@ -91,7 +84,7 @@ void	test_builtins(void)
 	if (ft_strncmp(line, prompt, ft_strlen(prompt)))
 	{
 		ft_putendl_fd(RED"Test 1 prompt failed", 1);
-		ft_printf("line:\t\t%s\nCorrect prompt:\t%s\n", line, prompt);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, prompt);
 	}
 	else
 		ft_putendl_fd(GREEN"Test 1 prompt success", 1);
@@ -104,7 +97,10 @@ void	test_builtins(void)
 	cwd = getcwd(NULL, 0);
 
 	if (ft_strncmp(line, cwd, ft_strlen(cwd)))
+	{
 		ft_putendl_fd(RED"Test 1 pwd failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, cwd);
+	}
 	else
 		ft_putendl_fd(GREEN"Test 1 pwd success", 1);
 	ft_free_n_null((void **)&line);
@@ -119,7 +115,10 @@ void	test_builtins(void)
 	prompt = get_prompt();
 
 	if (ft_strncmp(line, prompt, ft_strlen(prompt)))
+	{
 		ft_putendl_fd(RED"Test 2 prompt failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, prompt);
+	}
 	else
 		ft_putendl_fd(GREEN"Test 2 prompt success", 1);
 	ft_free_n_null((void **)&line);
@@ -131,7 +130,10 @@ void	test_builtins(void)
 	cwd = getcwd(NULL, 0);
 
 	if (ft_strncmp(line, cwd, ft_strlen(cwd)))
+	{
 		ft_putendl_fd(RED"Test 2 pwd failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, cwd);
+	}
 	else
 		ft_putendl_fd(GREEN"Test 2 pwd success", 1);
 	ft_free_n_null((void **)&line);
@@ -144,7 +146,10 @@ void	test_builtins(void)
 	line = get_next_line(read_output_fd);
 
 	if (ft_strncmp(line, "Hello World", ft_strlen("Hello World")))
+	{
 		ft_putendl_fd(RED"Test 1 echo failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, "Hello World");
+	}
 	else
 		ft_putendl_fd(GREEN"Test 1 echo success", 1);
 	ft_free_n_null((void **)&line);
@@ -157,7 +162,10 @@ void	test_builtins(void)
 	
 	
 	if (ft_strncmp(line, "-nnnnnnnnnm Hello World", ft_strlen("-nnnnnnnnnm Hello World")))
+	{
 		ft_putendl_fd(RED"Test 2 echo failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, "-nnnnnnnnnm Hello World");
+	}
 	else
 		ft_putendl_fd(GREEN"Test 2 echo success", 1);
 	ft_free_n_null((void **)&line);
@@ -172,7 +180,10 @@ void	test_builtins(void)
 	ft_free_n_null((void **)&prompt);
 
 	if (ft_strncmp(line, echo_line, ft_strlen(echo_line)))
+	{
 		ft_putendl_fd(RED"Test 3 echo failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, echo_line);
+	}
 	else
 		ft_putendl_fd(GREEN"Test 3 echo success", 1);
 	ft_free_n_null((void **)&line);
@@ -186,7 +197,10 @@ void	test_builtins(void)
 	ft_free_n_null((void **)&prompt);
 
 	if (ft_strncmp(line, echo_line, ft_strlen(echo_line)))
+	{
 		ft_putendl_fd(RED"Test 4 echo failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, echo_line);
+	}
 	else
 		ft_putendl_fd(GREEN"Test 4 echo success", 1);
 	ft_free_n_null((void **)&line);
@@ -197,7 +211,10 @@ void	test_builtins(void)
 	line = get_next_line(read_output_fd);
 
 	if (ft_strncmp(line, "Hello World-n", ft_strlen("Hello World-n")))
+	{
 		ft_putendl_fd(RED"Test 5 echo failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, "Hello World-n");
+	}
 	else
 		ft_putendl_fd(GREEN"Test 5 echo success", 1);
 	ft_free_n_null((void **)&line);
@@ -209,22 +226,78 @@ void	test_builtins(void)
 	line = get_next_line(read_output_fd);
 
 	if (ft_strncmp(line, "Hello World -n", ft_strlen("Hello World -n")))
+	{
 		ft_putendl_fd(RED"Test 6 echo failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, "Hello World -n");
+	}
 	else
 		ft_putendl_fd(GREEN"Test 6 echo success", 1);
 	ft_free_n_null((void **)&line);
 
 
-	//_ CHECKING EXIT TEST 1 _//
+	//_ CHECKING ENV TEST 1 _//
 	line = get_next_line(read_output_fd);
 	ft_free_n_null((void **)&line);
+	i = -1;
+
+	while (environ[++i])
+	{
+		line = get_next_line(read_output_fd);
+		if (ft_strncmp(line, environ[i], ft_strlen(environ[i])))
+			ft_printf(RED"Output:\t%s\nExpected:\t%s\n\n", line, environ[i]);
+		ft_free_n_null((void **)&line);
+	}
+	if (!environ[i])
+		ft_putendl_fd(GREEN"Test 1 env success", 1);
+	else
+		ft_putendl_fd(RED"Test 1 env failed", 1);
+
+
+	//_ CHECKING EXIT TEST 1 _//
 	line = get_next_line(read_output_fd);
+	tmp_line = get_next_line(read_output_fd);
+	while (line && tmp_line)
+	{
+		ft_free_n_null((void **)&line);
+		line = get_next_line(read_output_fd);
+		if (!line || !*line)
+			break;
+		ft_free_n_null((void **)&tmp_line);
+		tmp_line = get_next_line(read_output_fd);
+		if (!*tmp_line)
+			break;
+	}
+	if (!line || !*line)
+	{
+		if (line)
+			ft_free_n_null((void **)&line);
+		line = tmp_line;
+	}
 
 	if (ft_strncmp(line, "exit", ft_strlen("exit")))
+	{
 		ft_putendl_fd(RED"Test 1 exit failed", 1);
+		ft_printf("Output:\t%s\nExpected:\t%s\n\n", line, "exit");
+	}
 	else
 		ft_putendl_fd(GREEN"Test 1 exit success", 1);
 	ft_free_n_null((void **)&line);
+
+
+//_ CHECKING EXIT STATUS TEST 1 _//
+	if (WIFEXITED(status))
+	{
+		if (WEXITSTATUS(status) == 123)
+			ft_putendl_fd(GREEN"Test 1 exit status success", 1);
+		else
+		{
+			ft_printf(RED"Output:\t%d\nExpected:\t%d\n\n", WEXITSTATUS(status), 123);
+			ft_putendl_fd(RED"Test 1 exit status failed", 1);
+		}
+	}
+	else
+		ft_putendl_fd("Didn't exit properly", 1);
+
 
 	chdir("minishell");
 	kill(pid, SIGKILL);
