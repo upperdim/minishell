6b5112aeb7d94b682925bc98348253cc52b76689
JorFik <138692812+JorFik@users.noreply.github.com>
2024-03-29 16:58:09 +0100

    Added format and feedback to tty

--- unit_tests/test_parser.c
+++ unit_tests/test_parser.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 17:45:26 by JFikents          #+#    #+#             */
-/*   Updated: 2024/03/29 15:06:20 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/03/29 16:57:11 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(void)
 	result = parse_line("");
 	if (result)
 	{
-		ft_printf("parse_line(\"\"): Failed\n");
+		ft_printf("\nTest for parse_line(\"\"): Failed\n");
 		ft_printf("Outputs: %p\nExpected: NULL\n", result);
 		free(result);
 		result = NULL;
@@ -29,9 +29,7 @@ int	main(void)
 	system("\
 if [ $(leaks a.out | grep \"leaks for \" | awk '{print $3}') != 0 ]; then\n\
 	leaks a.out | grep \"Process \">> logs/result_parser.log\n\
-	echo \"Leaks found in parse_line(\\\"\\\")\" >> logs/result_parser.log\n\
-else\n\
-	echo \"\nNo leaks found in parse_line(\\\"\\\")\" >> logs/result_parser.log\n\
+	echo \"Failed leak test in parse_line(\\\"\\\")\" >> logs/result_parser.log\n\
 fi");
 	return (0);
 }
--- unit_tests/utils_functions.sh
+++ unit_tests/utils_functions.sh
@@ -21,3 +21,16 @@ function check_norminette
 		echo -e $RED && norminette | grep -E "Error:|Error!" && echo -e $DEFAULT
 	fi
 };
+
+function feedback
+{
+	if [ -z "$(grep "Failed" logs/result_parser.log)" ]; then
+	echo -e "\x1b[1;32mAll tests passed successfully.\x1b[0m"
+else
+	echo -e "\x1b[1;31m1 or more test failed:\x1b[0m"
+	(echo -ne "\x1b[0;31m"
+	grep "Failed" logs/result_parser.log
+	echo -ne "\x1b[0m") | awk '{print "\t", $0}'
+	echo -e "\x1b[34mCheck Logs for more information\x1b[0m"
+fi
+};
