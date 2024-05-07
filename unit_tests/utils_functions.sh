226fe3c0a4baa03ab263d962ba630efe44d40cb7
JorFik <138692812+JorFik@users.noreply.github.com>
2024-05-07 20:42:08 +0200

    Trying to solve unary error output

--- unit_tests/utils_functions.sh
+++ unit_tests/utils_functions.sh
@@ -6,14 +6,15 @@ RM="rm -rf"
 
 function prepare_logs_dir
 {
-	if [ ! -d "logs" ]; then
+	if [[ ! -d "logs" ]]; then
 		mkdir -p logs
 	fi
 };
 
 function check_norminette
 {
-	if [ -n "$(norminette | grep -E "Error:|Error!")" ]; then
+	normerror=$(norminette | grep -E "Error:|Error!")
+	if [ -n "$normerror" ]; then
 		echo -e "\x1b[1;31mNorminette error(s) detected, please fix it/them before running the pushing to vogsphere.\x1b[0m"
 		echo -e $RED && norminette | grep -E "Error:|Error!" && echo -e $DEFAULT
 	fi
@@ -21,13 +22,14 @@ function check_norminette
 
 function feedback
 {
-	if [ -z "$(grep "Failed" logs/result_parser.log)" ]; then
-	echo -e "\x1b[1;32mAll tests passed successfully.\x1b[0m"
-else
+	failed=$(grep "Failed" logs/result_parser.log)
+	if [ -z "$failed" ]; then
+		echo -e "\x1b[1;32mAll tests passed successfully.\x1b[0m"
+		return
+	fi
 	echo -e "\x1b[1;31m1 or more test failed:\x1b[0m"
 	(echo -ne "\x1b[0;31m"
 	grep "Failed" logs/result_parser.log
 	echo -ne "\x1b[0m") | awk '{print "\t", $0}'
 	echo -e "\x1b[34mCheck Logs for more information\x1b[0m"
-fi
 };
