ebc889bd70acb9b2a2f4f74b89da7284ba685cfa
JorFik <138692812+JorFik@users.noreply.github.com>
2024-05-07 20:41:34 +0200

    Added cmd capabilities for parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 14:26:29 by tunsal            #+#    #+#             */
-/*   Updated: 2024/03/29 18:53:28 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/05/07 17:20:22 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,19 @@ t_instruction	*parse_line(char *input)
 {
 	t_instruction	*instruction;
 
+	input = ft_strtrim(input, " ");
 	if (!input || !*input)
+	{
+		if (input)
+			free(input);
 		return (NULL);
+	}
 	instruction = ft_calloc(1, sizeof(t_instruction));
-	return (instruction);
+	if (!instruction)
+		return (free(input), NULL);
+	if (ft_strchr(input, ' '))
+		instruction->cmd = ft_substr(input, 0, ft_strchr(input, ' ') - input);
+	else
+		instruction->cmd = ft_strdup(input);
+	return (free(input), instruction);
 }
