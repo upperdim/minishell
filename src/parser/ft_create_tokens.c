f99b378de344fe67c3289d85947021cfa931e83a
JorFik <138692812+JorFik@users.noreply.github.com>
2024-05-13 21:44:37 +0200

    Unit test 4 Pass
    1 leak in test 4, not detected by leaks in Macos but detected with valgrind

--- src/parser/ft_create_tokens.c
+++ src/parser/ft_create_tokens.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:29:54 by JFikents          #+#    #+#             */
-/*   Updated: 2024/05/13 15:32:19 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/05/13 21:42:12 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int	ft_handle_quotes(char *input, t_split *new)
 	char	*prev_result;
 	int		index;
 
-	index = ft_count_spaces(input);
+	index = 0;
 	closing_quote = ft_strchr(&input[index + 1], input[index]);
 	prev_result = new->result;
 	if (closing_quote == NULL)
@@ -32,10 +32,10 @@ static int	ft_handle_quotes(char *input, t_split *new)
 	}
 	quoted_str = ft_substr(&input[index], 1, closing_quote - &input[index] - 1);
 	new->result = ft_strjoin(prev_result, quoted_str);
-	ft_free_n_null((void **)&quoted_str);
-	ft_free_n_null((void **)&prev_result);
 	if (*closing_quote == '\"' && ft_strchr(quoted_str, '$'))
 		ft_expand_env_var(input, index, new, closing_quote);
+	ft_free_n_null((void **)&quoted_str);
+	ft_free_n_null((void **)&prev_result);
 	return (index = closing_quote + 1 - input);
 }
 
@@ -56,7 +56,7 @@ static int	ft_check_4_word(char *input, t_split *new)
 			return (-1);
 		idx += check;
 	}
-	limiter = ft_find_limit(input);
+	limiter = ft_find_limit(&input[idx]);
 	old_token = new->result;
 	new_token = ft_substr(&input[idx], 0, limiter - &input[idx]);
 	if (!new_token)
