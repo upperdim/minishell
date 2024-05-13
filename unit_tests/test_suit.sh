4adb350cb3dc7873e5fca99bd295153da46dde68
Jorge Fikentscher <138692812+JorFik@users.noreply.github.com>
2024-05-13 20:06:20 +0000

    Added debug capabilities and valgrind if linux

--- unit_tests/test_suit.sh
+++ unit_tests/test_suit.sh
@@ -6,12 +6,13 @@ CFLAGS="-Wall -Wextra -Werror -Wunreachable-code"
 INCLUDES="-Iincludes -Ilib/libft/includes -Iunit_tests/includes"
 LIBRARIES="-Llib/libft -lft"
 DEBUG="-g3"
-if [[ "$1" == "d" ]]; then
+if [[ "$1" == "d" || "$2" == "d" ]]; then
 	CFLAGS="$CFLAGS $DEBUG"
 	echo -e "\x1b[1;33mDebug mode enabled\x1b[0m"
 fi
 if [[ "$(uname)" == "Linux" ]]; then
 	MANUALLY_ADDED_LIBFT="lib/libft/libft.a"
+	VALGRIND="valgrind --leak-check=full ./a.out"
 fi
 
 
@@ -22,6 +23,7 @@ function run_parser_test_1
 	for TEST_NUMBER in {1..3};
 	do
 	$CC $CFLAGS $INCLUDES $LIBRARIES $PARSER_FILES unit_tests/test_parser.c -DTEST=$TEST_NUMBER $MANUALLY_ADDED_LIBFT
+	$VALGRIND
 	./a.out >> logs/result_parser.log
 	done
 }
@@ -31,18 +33,21 @@ function run_parser_test_2
 	for TEST_NUMBER in {4..4};
 	do
 	$CC $CFLAGS $INCLUDES $LIBRARIES $PARSER_FILES unit_tests/test_parser_2.c -DTEST=$TEST_NUMBER $MANUALLY_ADDED_LIBFT
+	$VALGRIND
 	./a.out >> logs/result_parser.log
 	done
 }
 
-check_norminette
-prepare_logs_dir
-make test
-./test
-make -s lib/libft/libft.a
-echo -e "====\t\t$(date +%d\ %b\ %Y\ @\ %T)\t\t====" > logs/result_parser.log
-run_parser_test_1
-run_parser_test_2
-echo -e "====\t\t\tEND of the log\t\t\t====" >> logs/result_parser.log
-feedback
-$RM a.out
+if [[ $1 != "SOURCE" ]]; then
+	check_norminette
+	prepare_logs_dir
+	make test
+	./test
+	make -s lib/libft/libft.a
+	echo -e "====\t\t$(date +%d\ %b\ %Y\ @\ %T)\t\t====" > logs/result_parser.log
+	run_parser_test_1
+	run_parser_test_2
+	echo -e "====\t\t\tEND of the log\t\t\t====" >> logs/result_parser.log
+	feedback
+	$RM a.out
+fi
