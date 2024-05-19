a34fef012e92778085f8eb63f0b4efe2428aad66
Jorge Fikentscher <138692812+JorFik@users.noreply.github.com>
2024-05-19 16:39:08 +0000

    Added unit test 10

--- unit_tests/test_parser_2.c
+++ unit_tests/test_parser_2.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:50:49 by JFikents          #+#    #+#             */
-/*   Updated: 2024/05/19 14:21:55 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/05/19 16:37:01 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,6 +93,30 @@ int	main(void)
 	const char		*expected[2] = {"HelloWorld", NULL};
 	const char		*input = "echo Hello\"World\"       ";
 	const char		*leaks_line = "parse_line(\"echo Hello\"World\"       \")";
+
+	result = parse_line((char *) input);
+	error = ft_check_output(result, expected);
+	if (error == NO_ERROR)
+		ft_printf("Test for parse_line(\"%s\"): PASS\n", input);
+	else
+	{
+		ft_printf("\nTest for parse_line(\"%s\"): Failed\n", input);
+		ft_feedback(error, result, expected);
+	}
+	if (result)
+		ft_free_results(&result);
+	return (run_leaks(leaks_line), 0);
+}
+
+#elif TEST == 10
+
+int	main(void)
+{
+	t_instruction	*result;
+	int				error;
+	const char		*expected[2] = {"HelloWorldstuck", NULL};
+	const char		*input = "echo Hello\"World\"'stuck'";
+	const char		*leaks_line = "parse_line(\"echo Hello\"World\"'stuck'\")";
 
 	result = parse_line((char *) input);
 	error = ft_check_output(result, expected);
