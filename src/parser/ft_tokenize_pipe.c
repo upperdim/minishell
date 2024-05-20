6ab6e995b150ffe139ee5fc90db9eaa588238fcd
JorFik <138692812+JorFik@users.noreply.github.com>
2024-05-20 14:20:15 +0200

    Added ft_tonkeize_pipe.c

--- src/parser/ft_tokenize_pipe.c
+++ src/parser/ft_tokenize_pipe.c
@@ -3,20 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tokenize_pipe.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 22:26:52 by JFikents          #+#    #+#             */
-/*   Updated: 2024/05/18 22:31:09 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:19:18 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_tokenize_pipe(t_split *token)
+t_split	*ft_tokenize_pipe(t_split *token)
 {
 	char	*pipe;
+	t_split	*new_tokens;
+	// char	**pipe_split;
+	// int		pipe_count;
 
 	pipe = ft_strchr(token->result, '|');
-	ft_printf("prev result: %s\n", token->prev->result);
-	return ;
+	*pipe = '\0';
+	new_tokens = ft_calloc(1, sizeof(t_split));
+	if (!new_tokens)
+		return (NULL);
+	// new_tokens->result = ft_substr();
+	// pipe_split = ft_split(token->result, '|');
+	// pipe_count = 0;
+	// while (pipe_split[pipe_count])
+	// 	pipe_count ++;
+	return token;
 }
