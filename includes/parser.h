b92e1a99fa001ee5191cf15dc673049a817003a4
upperdim <45048352+upperdim@users.noreply.github.com>
2024-05-29 21:35:09 +0200

    make master compilable

--- includes/parser.h
+++ includes/parser.h
@@ -6,24 +6,40 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 14:11:30 by tunsal            #+#    #+#             */
-/*   Updated: 2024/03/12 15:31:01 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/05/29 19:36:30 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-enum	e_token
+# include <stdbool.h>
+
+typedef enum	e_token_type
 {
-	CMD,
-	ARG,
+	STRING,
+	REDIR_TO,
+	APPEND_TO,
+	REDIR_FROM,
+	HERE_DOC,
 	PIPE,
-	REDIR_OVER_L,
-	REDIR_OVER_R,
-	REDIR_APPD_L,
-	REDIR_APPD_R,
-	QUOTE_S,
-	QUOTE_D
-};
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+	struct s_token	*prev;
+}	t_token;
+
+
+t_token			*parse_line(char *line);
+t_token			*ft_lexer(char *input);
+void			ft_free_link_list(t_token *split);
+void			ft_expand_env_var(t_token *token);
+int				ft_move_to_next(char *input, t_token **new);
+void			ft_look_for_tokens_in_strs(t_token *head);
+t_token			*ft_tokenize_pipe(t_token *token);
 
 #endif
