c189b9db5a7bd418cc24d20caf565bbf40fec247
JorFik <138692812+JorFik@users.noreply.github.com>
2024-06-04 21:52:27 +0200

    Add feature: pass unit test 1 for the create argvs

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 17:01:28 by JFikents          #+#    #+#             */
-/*   Updated: 2024/06/01 15:44:04 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/06/04 15:40:26 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,40 @@
 // 			status = WEXITSTATUS(status);
 // 	}
 // }
+
+char	**create_argvs(t_token *token)
+{
+	char	**argv;
+	int		argc;
+
+	argc = 0;
+	while (token->next != NULL && ++argc)
+		token = token->next;
+	argv = ft_calloc(argc + 1, sizeof(char *));
+	if (argv == NULL)
+		return (NULL);
+	while (token->prev != NULL)
+		token = token->prev;
+	argc = 0;
+	while (token != NULL)
+	{
+		argv[argc++] = token->value;
+		token = token->next;
+		if (token != NULL)
+			ft_free_n_null((void **)&token->prev);
+	}
+	return (argv);
+}
+
+int	exec(t_token *token)
+{
+	char	**argv;
+	int		pipe_fd[2];
+
+	argv = create_argvs(token);
+	if (argv == NULL)
+		return (1);
+	(void)argv;
+	(void)pipe_fd;
+	return (0);
+}
