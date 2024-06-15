7e3bbd547147ad3d212ee2f0f1e7c196d01e9593
JorFik <138692812+JorFik@users.noreply.github.com>
2024-06-15 14:17:16 +0200

    Feature (Return):
    Added return to isolate_token, so we don't lose the other tokens

--- src/exec/token_utils.c
+++ src/exec/token_utils.c
@@ -6,35 +6,48 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 20:49:08 by JFikents          #+#    #+#             */
-/*   Updated: 2024/06/11 20:49:22 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/06/14 14:40:49 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	isolate_token(t_token *token)
+/**
+ * @brief Isolates a token from the linked list.
+ * 
+ * @param token the token to isolate
+ * @return t_token* the next token after the isolated token or if there is no
+ * 	next token, the previous token, or NULL if there is no previous token either.
+ */
+t_token	*isolate_token(t_token **token)
 {
-	if (token->prev)
-		token->prev->next = token->next;
-	token->prev = NULL;
-	if (token->next)
-		token->next->prev = token->prev;
-	token->next = NULL;
+	t_token	*isolated_token;
+
+	isolated_token = *token;
+	*token = (*token)->next;
+	if (isolated_token->prev)
+		isolated_token->prev->next = isolated_token->next;
+	isolated_token->prev = NULL;
+	if (isolated_token->next)
+		isolated_token->next->prev = isolated_token->prev;
+	isolated_token->next = NULL;
+	return (isolated_token);
 }
 
-void	add_token_last(t_token **head, t_token *new)
+void	add_token_last(t_token **head, t_token **new)
 {
 	t_token	*last;
+	t_token	*isolated_token;
 
+	isolated_token = isolate_token(new);
 	if (*head == NULL)
 	{
-		*head = new;
+		*head = isolated_token;
 		return ;
 	}
 	last = *head;
 	while (last->next != NULL)
 		last = last->next;
-	isolate_token(new);
-	last->next = new;
-	new->prev = last;
+	last->next = isolated_token;
+	isolated_token->prev = last;
 }
