06a81cfa097dffd7cb7556fd0262ca8f3f05c20d
JorFik <138692812+JorFik@users.noreply.github.com>
2024-06-15 18:30:30 +0200

    Added (free_cmd):
    Created function to properly free the cmd structure generated

---
+++ src/exec/exec_utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/15 17:15:16 by JFikents          #+#    #+#             */
+/*   Updated: 2024/06/15 18:25:43 by JFikents         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	free_cmd(t_cmd **cmd)
+{
+	t_cmd	*tmp;
+
+	while (*cmd != NULL)
+	{
+		tmp = *cmd;
+		*cmd = (*cmd)->next;
+		ft_free_2d_array((void ***)&tmp->argv, FREE_ANY_SIZE);
+		ft_free_link_list(tmp->strs);
+		ft_free_link_list(tmp->redirects);
+		ft_free_n_null((void **)&tmp);
+	}
+}
