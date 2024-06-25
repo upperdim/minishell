fefbd88346105b21a10ca8a925930f1cdad6fff0
JorFik <138692812+JorFik@users.noreply.github.com>
2024-06-25 15:17:47 +0200

    Add (trigger_here_doc):
    added to h file

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 12:20:20 by JFikents          #+#    #+#             */
-/*   Updated: 2024/06/24 20:24:37 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/06/25 13:33:45 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,6 @@ void	ft_free_link_list(t_token *split);
 // Execution utilities
 void	free_cmd(t_cmd **cmd);
 int		set_redir(t_token *redirections);
+int		trigger_here_doc(char *limiter);
 
 #endif
