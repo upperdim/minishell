a12211fdef8c5715673db3cfb0e2e6f5b1370501
JorFik <jfikents@student.42Heilbronn.de>
2024-07-01 23:02:39 +0200

    Feature (export):
    first commit with export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,11 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 15:18:14 by JFikents          #+#    #+#             */
-/*   Updated: 2024/03/27 13:20:34 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/07/01 18:32:42 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	*add_env_var(char *var)
+{
+	char	*value;
+	char	*key;
+	char	*tmp;
+
+	value = ft_strchr(var, '=');
+	if (value == NULL)
+		return (ft_putstr_fd("minishell: export: invalid argument", 2), NULL);
+	key = ft_substr(var, 0, value - var);
+	if (key == NULL)
+		return (ft_putstr_fd(E_ALLOC, 2), NULL);
+	tmp = ft_strjoin(key, "=");
+	if (tmp == NULL)
+		return (ft_putstr_fd(E_ALLOC, 2), NULL);
+	if (ft_getenv(key) == NULL)
+		ft_addenv(tmp);
+	else
+		ft_setenv(tmp);
+	ft_free_n_null((void **)&key);
+	ft_free_n_null((void **)&tmp);
+	return (NULL);
+}
