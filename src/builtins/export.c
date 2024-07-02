3f7c6bb1a5c83444eedc3b78ed1d8cbf04503a45
JorFik <jfikents@student.42Heilbronn.de>
2024-07-02 18:11:13 +0200

    Feature (make_env_bigger)
    Function added to reallocate environ to add a new environ

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,17 +6,42 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 15:18:14 by JFikents          #+#    #+#             */
-/*   Updated: 2024/07/01 18:32:42 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/07/02 15:46:40 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	*make_env_bigger(char *var)
+{
+	extern char	**environ;
+	char		**new_environ;
+	int			i;
+
+	i = 0;
+	while (environ[i])
+		i++;
+	new_environ = ft_calloc(sizeof(char *), (i + 2));
+	if (new_environ == NULL)
+		return (ft_putstr_fd(E_ALLOC, 2), NULL);
+	i = -1;
+	while (environ[++i])
+		new_environ[i] = environ[i];
+	new_environ[i] = ft_strdup(var);
+	if (new_environ[i] == NULL)
+		return (ft_putstr_fd(E_ALLOC, 2), NULL);
+	new_environ[i + 1] = NULL;
+	free_2d_array((void **)environ, FREE_ANY_SIZE);
+	environ = new_environ;
+	return (environ[i]);
+}
+
 void	*add_env_var(char *var)
 {
-	char	*value;
-	char	*key;
-	char	*tmp;
+	const char	*value = ft_strchr(var, '=');
+	char		*key;
+	int			i;
+	extern char	**environ;
 
 	value = ft_strchr(var, '=');
 	if (value == NULL)
