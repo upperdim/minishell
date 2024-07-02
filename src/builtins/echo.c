44b003054d2d92af82bc893d2eb8b3ac89bea646
JorFik <jfikents@student.42Heilbronn.de>
2024-07-02 17:51:47 +0200

    Cleaning (Legacy code)

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,32 +6,11 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 14:57:51 by JFikents          #+#    #+#             */
-/*   Updated: 2024/07/01 15:53:05 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/07/02 14:36:48 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static char	*echo_parser(char *input)
-{
-	char	*parsed_input;
-	int		i;
-
-	i = 0;
-	parsed_input = input;
-	if (parsed_input[0] == '-')
-	{
-		i++;
-		while (parsed_input[i] == 'n')
-			i++;
-		if (parsed_input[i] == ' ' || parsed_input[i] == '\0')
-			parsed_input += i;
-	}
-	if (parsed_input != input)
-		while (*parsed_input == ' ')
-			parsed_input++;
-	return (parsed_input);
-}
 
 static int	check_for_n_flag(char **input)
 {
@@ -66,25 +45,3 @@ void	echo(char **input)
 	if (!flag_n)
 		ft_putstr_fd("\n", 1);
 }
-// {
-// 	int		space_is_present;
-// 	char	*parsed_input;
-// 	int		flag_n;
-
-// 	space_is_present = 0;
-// 	flag_n = 0;
-// 	while (*input == ' ')
-// 	{
-// 		input++;
-// 		space_is_present = 1;
-// 	}
-// 	parsed_input = echo_parser(input);
-// 	if (parsed_input != input && ++flag_n)
-// 		input = parsed_input;
-// 	if (*input && space_is_present)
-// 		ft_putstr_fd(input, 1);
-// 	else if (*input)
-// 		ft_printf_fd(2, "minishell: echo%s: command not found", input);
-// 	if (!flag_n)
-// 		ft_putstr_fd("\n", 1);
-// }
