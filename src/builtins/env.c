0eb248c32ef06b94c3690a48657ff4cfabcf337a
JorFik <jfikents@student.42Heilbronn.de>
2024-07-02 18:09:07 +0200

    Fix (env):
    printed more environ than needed

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -22,7 +22,8 @@ void	env(const int argc)
 		ft_printf_fd(2, "minishell: env: Too many arguments\n");
 		return ;
 	}
-	i = 0;
-	while (environ[i])
-		ft_putendl_fd(environ[i++], 1);
+	i = -1;
+	while (environ[++i])
+		if (ft_strchr(environ[i], '=') != NULL)
+			ft_putendl_fd(environ[i], 1);
 }
