51182f4a603fdf6dfd6f0c5a71c05101d75f5a81
JorFik <138692812+JorFik@users.noreply.github.com>
2024-07-10 21:19:38 +0200

    Fix (cd -)

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,28 +6,25 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 13:28:29 by JFikents          #+#    #+#             */
-/*   Updated: 2024/07/10 15:01:49 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/07/10 21:13:26 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	set_oldpwd(void)
+static int	set_as_oldpwd(const char *pwd)
 {
-	const char	*pwd = getcwd(NULL, 0);
 	char		*oldpwd;
 
 	if (pwd == NULL)
 		return (ft_printf_fd(2, ERROR_MSG_PERROR, "cd"), perror(NULL), 1);
 	oldpwd = ft_strjoin("OLDPWD=", pwd);
 	if (oldpwd == NULL)
-		return (ft_free_n_null((void **)&pwd), ft_printf_fd(2, ERROR_MSG,
-				"OLDPWD", E_ALLOC), EXIT_FAILURE);
-	ft_free_n_null((void **)&pwd);
+		return (ft_printf_fd(2, ERROR_MSG, "OLDPWD", E_ALLOC), EXIT_FAILURE);
 	if (add_env_var(oldpwd) == EXIT_FAILURE)
 		return (ft_printf_fd(2, ERROR_MSG, "cd", "Error setting OLDPWD", 2),
-			free(oldpwd), 1);
-	return (free(oldpwd), 0);
+			free(oldpwd), EXIT_FAILURE);
+	return (free(oldpwd), EXIT_SUCCESS);
 }
 
 static int	cd_oldpwd(void)
@@ -43,20 +40,21 @@ static int	cd_oldpwd(void)
 
 int	cd(const int argc, char **argv)
 {
-	int	status;
+	const char	*pwd = getenv("PWD");
+	int			status;
 
 	status = 0;
 	if (argc > 2)
 		return (ft_printf_fd(2, ERROR_MSG, "cd", "too many arguments"),
 			EXIT_FAILURE);
-	if (set_oldpwd() == 1)
-		return (EXIT_FAILURE);
 	if (argc == 1 || ft_strncmp(argv[1], "~\0", 2) == 0)
 		status = chdir(getenv("HOME"));
 	if (ft_strncmp(argv[1], "-\0", 2) == 0)
 		status = cd_oldpwd();
 	else
 		status = chdir(argv[1]);
+	if (set_as_oldpwd(pwd) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	if (status == -1)
 	{
 		ft_printf_fd(2, ERROR_MSG_PERROR"%s: ", "cd", argv[1]);
