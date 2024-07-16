3b5d139cecc7ac12f3f08da5e413eb614ff4e737
upperdim <45048352+upperdim@users.noreply.github.com>
2024-07-16 18:46:16 +0200

    initiated tokenizer and added notes

---
+++ src/parser/tokenizer/tokenizer.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 03:53:39 by tunsal            #+#    #+#             */
+/*   Updated: 2024/07/16 18:45:43 by tunsal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_token	*tokenizer(char *line)
+{
+	t_token	*head;
+	(void) line;
+	head = NULL;
+	// append s2 to s1, updating s1
+	// get_next_line gnl_joinfree() or similar for python append()
+	//
+	// a util for appending char 
+	return (head);
+}
