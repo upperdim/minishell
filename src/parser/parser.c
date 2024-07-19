b7b3cb23812738161704276b9b4b3889f27e5efa
JorFik <138692812+JorFik@users.noreply.github.com>
2024-07-19 13:58:27 +0200

    Fix (premature ending)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 14:26:29 by tunsal            #+#    #+#             */
-/*   Updated: 2024/07/19 06:30:52 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/07/18 21:34:08 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,14 +49,16 @@ t_token	*parse(char *line)
 		return (NULL);
 	token_list = NULL;
 	if (validate_quotes(line) == FALSE)
-		return (ft_printf("minishell: SyntaxError: unclosed quotes\n"), NULL);
+		return (ft_printf_fd(2, "minishell: SyntaxError: unclosed quotes\n"),
+			NULL);
 	tilda_idxs_to_expand = NULL;
 	detect_tilda_expansions(line, &tilda_idxs_to_expand);
 	var_idxs_to_expand = NULL;
 	detect_var_expansions(line, &var_idxs_to_expand, 0);
 	token_list = tokenize(line);
 	if (!check_token_rules(token_list))
-		return (ft_printf("minishell: SyntaxError: invalid tokens\n"), NULL);
+		return (ft_printf_fd(2, "minishell: SyntaxError: invalid tokens"),
+			NULL);
 	expand_tilda(token_list, tilda_idxs_to_expand, list_get_size(tilda_idxs_to_expand));
 	expand_var(token_list, var_idxs_to_expand, list_get_size(var_idxs_to_expand));
 	if (!merge_quotes(token_list))
