21239bd0b8f2e0ed33393c97c17e6a95cf50190e
JorFik <138692812+JorFik@users.noreply.github.com>
2024-07-24 18:45:59 +0200

    Clean (testing stuff)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 08:42:53 by tunsal            #+#    #+#             */
-/*   Updated: 2024/07/21 21:26:04 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/07/24 18:45:26 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,16 +77,6 @@ static char	*get_input(void)
 	ft_free_n_null((void **)&prompt);
 	return (input);
 }
-// !USE for testing
-	// if (isatty(fileno(stdin)))
-	// 	input = readline(prompt);
-	// else
-	// {
-	// 	char	*line;
-	// 	line = get_next_line(fileno(stdin));
-	// 	input = ft_strtrim(line, "\n");
-	// 	free(line);
-	// }
 
 int	main(void)
 {
